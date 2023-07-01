2700e2f1da40fbcb338a2bff6698489f057843de
code-env <org.codeen@gmail.com>
2023-07-01 12:26:21 +0100

    this is it

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
               {item.links.map((link: links, index) => (
                 <li
                   key={index}
-                  className="font-sans font-normal text-white/80 text-xl"
+                  className="font-sans font-normal text-white/80 "
                 >
                   {link.path ? (
                     <Link href={`#${link.path}`}>{link.name}</Link>
@@ -43,7 +43,14 @@ const Footer = () => {
         ))}
         <div className="flex flex-col gap-2">
           <p className="font-bold text-2xl">Contact</p>
-          <ul className=""></ul>
+          <ul className="">
+            <li className="font-sans font-normal text-white/80">
+              Mail: contact@skaleway.com
+            </li>
+            <li className="font-sans font-normal text-white/80 ">
+              Phone: 6 54 58 46 24
+            </li>
+          </ul>
         </div>
       </div>
     </div>
