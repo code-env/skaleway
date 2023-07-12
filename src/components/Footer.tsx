01ac55b122e98b655cb3f9723ccc3fbab2a9b261
code-env <org.codeen@gmail.com>
2023-07-12 16:33:18 +0100

    made some changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,40 +12,50 @@ type links = {
 const Footer = () => {
   const footerRoutes = useFooterRoutes();
 
+  const currentYear = Date.now();
+
   return (
-    <footer className="bg-secondary h-96 responsive-px text-white py-10 max-[700px]:h-fit flex gap-[100px] max-[700px]:flex-col">
-      <Logo />
-      <div className="flex-1 flex justify-between max-[700px]:flex-col">
-        {footerRoutes.map((item, index) => (
-          <div key={index} className="flex flex-col gap-2">
-            <p className="font-bold text-2xl">{item.name}</p>
-            <ul className="list-disc mx-7 leading-[160%]">
-              {item.links.map((link: links, index) => (
-                <li
-                  key={index}
-                  className="font-sans font-normal text-white/80 "
-                >
-                  {link.path ? (
-                    <Link href={`#${link.path}`}>{link.name}</Link>
-                  ) : (
-                    <span>{link.name}</span>
-                  )}
-                </li>
-              ))}
+    <footer className="bg-secondary h responsive-px text-white py-20 h-fit gap-10 flex flex-col">
+      <div className="w-full flex gap-[100px] max-[700px]:flex-col">
+        <Logo />
+        <div className="flex-1 flex justify-between max-[700px]:flex-col">
+          {footerRoutes.map((item, index) => (
+            <div key={index} className="flex flex-col gap-2">
+              <p className="font-bold text-2xl">{item.name}</p>
+              <ul className="list-disc mx-7 leading-[160%]">
+                {item.links.map((link: links, index) => (
+                  <li
+                    key={index}
+                    className="font-sans font-normal text-white/80 "
+                  >
+                    {link.path ? (
+                      <Link href={`#${link.path}`}>{link.name}</Link>
+                    ) : (
+                      <span>{link.name}</span>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <div className="flex flex-col gap-2">
+            <p className="font-bold text-2xl">Contact</p>
+            <ul className="">
+              <li className="font-sans font-normal text-white/80">
+                Mail: contact@skaleway.com
+              </li>
+              <li className="font-sans font-normal text-white/80 ">
+                Phone: 6 54 58 46 24
+              </li>
             </ul>
           </div>
-        ))}
-        <div className="flex flex-col gap-2">
-          <p className="font-bold text-2xl">Contact</p>
-          <ul className="">
-            <li className="font-sans font-normal text-white/80">
-              Mail: contact@skaleway.com
-            </li>
-            <li className="font-sans font-normal text-white/80 ">
-              Phone: 6 54 58 46 24
-            </li>
-          </ul>
         </div>
+      </div>
+
+      <div className="w-full">
+        <p className="text-primary text-center">
+          {currentYear} All Rights Reserved - Skaleway
+        </p>
       </div>
     </footer>
   );
