9cad3de3331df2f7c5a2c2ea5effb8edf786662b
code-env <org.codeen@gmail.com>
2023-07-21 04:14:21 +0100

    working on the graph

---
+++ src/app/admin/components/ui/Card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="flex-1 p-10 rounded-lg bg-neutral shadow-inner transition-all duration-300 cursor-pointer">
+      Card
+    </div>
+  );
+};
+
+export default Card;
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Cards from "./components/ui/Cards";
 
 const Admin = () => {
-  return <div className="flex min-h-screen text-black w-full">Admin</div>;
+  return (
+    <div className="flex min-h-screen text-black w-full">
+      <Cards />
+    </div>
+  );
 };
 
 export default Admin;
