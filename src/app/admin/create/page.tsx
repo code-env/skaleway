0d40532239a4038f03048891d623b0dbd3ab692d
code-env <org.codeen@gmail.com>
2023-07-22 12:12:32 +0100

    added the enquiry form

--- src/app/admin/create/page.tsx
+++ src/app/admin/create/page.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Input from "@/components/Input";
+
+interface post {
+  username: string;
+  image?: string | null;
+  title: string;
+  description: string;
+}
 
 const Create = () => {
-  return <div>Create</div>;
+  const [userData, setUserData] = useState<post>({
+    username: "",
+    title: "",
+    image: null,
+    description: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+
+    setUserData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  const { username, title, image, description } = userData;
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-1/2 mx-auto border border-red-500 p-4 rounded-lg flex flex-col gap-4"
+    >
+      <Input
+        name="username"
+        onChange={handleChange}
+        value={username}
+        placeholder="username"
+      />
+      <Input
+        name="title"
+        onChange={handleChange}
+        value={title}
+        placeholder="Title"
+      />
+      <Input
+        name="description"
+        onChange={handleChange}
+        value={description}
+        placeholder="Description"
+        textarea
+      />
+    </form>
+  );
 };
 
 export default Create;
